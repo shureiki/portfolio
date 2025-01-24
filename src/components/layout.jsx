0927d428a26ef33671330f32a780d95e05b6ccb7
shureiki <lxnnylqs44@gmail.com>
2025-01-25 00:47:32 +0100

    Fix avatar display

--- src/components/layout.jsx
+++ src/components/layout.jsx
@@ -28,7 +28,7 @@ import useProjects from '@/hooks/useProjects';
 const UserAvatar = () => {
 	return (
 		<Box className='p-2'>
-			<img src='/avatar.jpg' alt='Avatar' />
+			<img src={userInfo.avatarPath} alt='Avatar' />
 		</Box>
 	);
 }
